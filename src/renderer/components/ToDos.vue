fa4035f76934cb8e2f7bc07aa412795c73cef123
Bruno Krebs <bruno.krebs@auth0.com>
2019-09-27 16:25:28 -0300

    init the to-do routes

---
+++ src/renderer/components/ToDos.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Welcome to the To-Dos Page</h2>
+    </div>
+</template>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'todos-page',
+      component: require('@/components/ToDos').default
     },
     {
       path: '*',
