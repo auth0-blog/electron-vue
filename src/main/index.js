be9ed797140c711c053c9b16dd2a029aacf72266
Bruno Krebs <bruno.krebs@auth0.com>
2019-09-27 14:50:54 -0300

    fixing the sample app to run with Electron 6;

--- src/main/index.js
+++ src/main/index.js
@@ -22,7 +22,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true
+    }
   })
 
   mainWindow.loadURL(winURL)
