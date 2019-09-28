d66cbb0e8a41eb0c4830df81353451423c38aa51
Bruno Krebs <bruno.krebs@auth0.com>
2019-09-27 22:14:35 -0300

    consuming the API;

--- src/renderer/components/ToDos.vue
+++ src/renderer/components/ToDos.vue
@@ -1,5 +1,41 @@
 <template>
     <div>
-        <h2>Welcome to the To-Dos Page</h2>
+        <div>
+            <div>
+                <button @click="fetchTodos()" class="btn btn-primary">Fetch Todos</button>
+            </div>
+        </div>
+        <div>
+            <div>
+                <ul>
+                    <li v-for="todo in todos" :key="todo.id">{{todo.task}}</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+  const axios = require('axios')
+  export default {
+    name: 'ToDos',
+    data: () => {
+      return {
+        todos: []
+      }
+    },
+    methods: {
+      async fetchTodos () {
+        axios
+          .get('http://localhost:1337/todos')
+          .then(response => {
+            console.log(response)
+            this.todos = response.data
+            console.log(this.todos)
+          })
+          .catch(error => {
+            if (error) throw new Error(error)
+          })
+      }
+    }
+  }
+</script>
